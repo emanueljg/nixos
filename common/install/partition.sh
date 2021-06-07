d4186b8a8ccc2a5833159f38c25d28a38a36f334
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2021-06-07 12:37:44 +0200

    Create partition.sh

---
+++ common/install/partition.sh
@@ -0,0 +1,15 @@
+sgdisk -Z $SDX
+sgdisk -n 0:0:+512MiB -t 0:ef00 -c 0:efi  $SDX
+sgdisk -n 0:0:+8GiB   -t 0:8200 -c 0:swap $SDX
+sgdisk -n 0:0:0       -t 0:8300 -c 0:root $SDX
+sgdisk -p $SDX
+partprobe
+
+mkfs.fat -F 32 -n efi  ${SDX}1
+mkswap         -L swap ${SDX}2
+mkfs.ext4      -L root ${SDX}3
+
+mount /dev/disk/by-label/root /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/efi /mnt/boot
+swapon ${SDX}2
