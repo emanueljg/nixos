cc13f2d299c68179e982e34bd9afaa5b556863b2
emanueljg <emanueljohnsongodin@gmail.com>
2022-06-05 20:11:42 +0200

    add new quickmarks

--- parts/home/programs/qutebrowser/quickmarks.nix
+++ parts/home/programs/qutebrowser/quickmarks.nix
@@ -4,13 +4,20 @@
   ];
   my.programs.qutebrowser.quickmarks = {
     f-d = "https://discord.com/channels/@me";
+
     y-hn = "https://www.youtube.com/watch?v=HUd_ikEGPPM&list=PLmJS4rAJemEaN6k5S0g43vDlSib1qWudz"; 
 
-    t-hm = "https://nix-community.github.io/home-manager/options.html";
-    t-pkgs = "https://search.nixos.org/packages";
-    t-nixlang = "https://teu5us.github.io/nix-lib.html#nix-builtin-functions";
-    t-qb-faq = "https://qutebrowser.org/FAQ.html";
-    t-dl-1337x = "https://www.1377x.to/";
+    t-no-opt = "https://search.nixos.org/options";
+    t-no-hm = "https://nix-community.github.io/home-manager/options.html";
+    t-no-pkgs = "https://search.nixos.org/packages";
+    t-no-lang = "https://teu5us.github.io/nix-lib.html#nix-builtin-functions";
 
+    t-qb-faq = "https://qutebrowser.org/FAQ.html";
+    t-qb-func = "https://qutebrowser.org/doc/help/commands.html";
+    t-qb-us = "https://qutebrowser.org/doc/userscripts.html";
+
+    t-dl-nsi = "https://nyaa.si/";
+    t-dl-rbg  = "https://rarbg.to/torrents.php";
+    t-dl-1337x = "https://www.1377x.to/";
   };
 }
