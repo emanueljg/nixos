e1ba82fc58b46fb6bf40027e4cb15fdff375384e
emanueljg <emanueljohnsongodin@gmail.com>
2022-06-21 17:07:04 +0200

    delete old dirs

--- hosts/seneca/deluge.nix
+++
@@ -1,7 +0,0 @@
-{ config, ... }:
-
-{
-  services.deluge = {
-    enable = true;
-  };
-}
--- lib/customOpts/hw/bluetooth.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, ... }:
-
-with lib; let
-  cfg = config.customOpts.boot;
-in {
-
-
-  options = {
-    method = m
