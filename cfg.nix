7f670ddda98ba6ce43d2b5ec628063edaed7eac7
emanueljg <emanueljohnsongodin@gmail.com>
2022-06-23 17:17:52 +0200

    add qutebrowser overlay

--- cfg.nix
+++ cfg.nix
@@ -38,6 +38,7 @@ rec {
   ;
 
   inherit (lib.strings)
+    concatStrings
     concatMapStrings
     removePrefix
     stringToCharacters
@@ -1285,6 +1286,66 @@ rec {
     #    };
         
       };
+    })
+
+    # qutebrowser-extra
+    # Sets some extra hardcoded python stuff by overlaying the postInstall and sedding away
+    (mkKnob "seneca" {
+      my.nixpkgs.overlays = 
+        let
+          # custom variables
+          activeDownloadFormat = "({index}) {name} | {total} | {perc:<2}% | {speed:<10}";
+          doneDownloadFormat = "({index}) {name} | {total} | {perc:<2}% | Finished!"; 
+
+          # inferred
+          setActiveDownloadsFormat = concatStrings [
+            # This format is defined over two lines in the original python class, 
+            # requiring two sed commands to fully change it.
+
+            ''cat ''
+              ''"qutebrowser/browser/downloads.py"''
+            
+            " | "
+
+            ''sed "s''
+                ''/{index}: {name} \[{speed:>10}|{remaining:>5}|{perc:>2}%|''
+                ''/${activeDownloadFormat}''
+            ''/g"''
+            
+            " | "
+
+            ''sed "s''
+                ''/{down}\/{total}\]''
+                ''/''  # delete
+            ''/g"''
+
+            # Set done downloads format
+            # sed "s/{index}: {name} \[{perc:>2}%|{total}\]/FOUND2/g"
+
+            " | "
+
+            ''sed "s''
+              ''/{index}: {name} \[{perc:>2}%|{total}\]''
+              ''/${doneDownloadFormat}''
+            ''/g"''
+              
+            " > "
+              
+            ''"$out/lib/python3.9/site-packages/qutebrowser/browser/downloads.py"''
+          ];
+
+
+
+        in
+          [(self: super: {
+              qutebrowser = super.qutebrowser.overrideAttrs (oldAttrs: rec {
+                postInstall = oldAttrs.postInstall + ''
+                  ${setActiveDownloadsFormat}
+                '';
+              });
+            }
+          )]
+      ;
     })
 
     # pkgs-qutebrowser
