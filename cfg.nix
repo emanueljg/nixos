0600c61c099491549225c8a23779743e3b350796
emanueljg <emanueljohnsongodin@gmail.com>
2022-06-26 08:45:43 +0200

    add pfetch

--- cfg.nix
+++ cfg.nix
@@ -1227,6 +1227,21 @@ rec {
         };
       };
     })
+  
+    # pkgs-pfetch
+    (mkKnob true {
+      my.home = {
+        packages = [ pkgs.pfetch ]; 
+        shellAliases."pf" = "echo && pfetch"; # inserts a newline before pfetch 
+        sessionVariables = {
+          PF_INFO = "ascii os host kernel wm editor pkgs memory";
+          PF_COL1 = 6;
+          PF_COL2 = 4; 
+          PF_COL3 = 4;
+        };
+      };
+      
+    })
 
     # pkgs-kitty 
     (mkKnob [ "seneca" ] {
