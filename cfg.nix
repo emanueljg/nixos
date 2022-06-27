d27c93deee7dcbb1b09e0d8c442f6628dc3ffc7a
emanueljg <emanueljohnsongodin@gmail.com>
2022-06-27 12:44:01 +0200

    test invidious out

--- cfg.nix
+++ cfg.nix
@@ -577,6 +577,26 @@ rec {
         };
       };
     })
+  
+    # srv-invidious
+    (mkKnob "aurelius" {
+      services.invidious.enable = true;
+      
+      # setup ports
+      services.invidious = {
+        port = 34030;
+        database.port = 34031;
+      };
+
+      networking.firewall.allowedTCPPorts = [ 
+        config.services.invidious.port
+        config.services.invidious.database.port
+      ];
+    
+      # configuration
+    
+      
+    })
 
     # Sound 
     (mkKnob true {
