6ffb09850c2ad0f613f2d5fa7946feb0ce1ede5d
emanueljg <emanueljohnsongodin@gmail.com>
2022-08-12 11:25:45 +0200

    fixes

--- cfg.nix
+++ cfg.nix
@@ -1113,26 +1113,7 @@ rec {
           # fingerprint = ???
           # primary = ???
         };
-    })
-  
-    # pkgs-zsh
-    (mkKnob [ "aurelius" "seneca" ] {
-      # sets default shell
-      users.users.ejg.shell = pkgs.zsh;
-
-      # required for
-      # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.enableCompletion
-      environment.pathsToLink = [ "/share/zsh" ];
-
-      my.programs.zsh = {
-        enable = true;
-        enableSyntaxHighlighting = true;
-        enableVteIntegration = true;
-        autocd = true;
-        # cdpath = [ ]  could try and add this later?
-        # defaultKeymap  perhaps this too
-        # dirHashes     definitely this, could be very useful
-      
+            
         workspaces = range 1 10;
 
         # old battery width = 135
