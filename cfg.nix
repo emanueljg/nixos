a61643e2ec1ab54fae6f913dc6d1a641244ab6c7
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2022-08-14 17:02:30 +0200

    Update cfg.nix

--- cfg.nix
+++ cfg.nix
@@ -1910,7 +1910,8 @@ rec {
     })
   
     (mkKnob [ "aurelius" "seneca" ] {
-      my.home.packages = with pkgs; {
+      # https://askubuntu.com/questions/314802/is-there-a-comprehensive-list-of-mupdf-keyboard-shortcuts
+      my.home.packages = with pkgs; [
         mupdf
       ];
     })
