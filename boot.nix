5240086e4f4abae512388019315a833c88a19bfe
emanueljg <emanueljohnsongodin@gmail.com>
2023-01-19 12:08:17 +0100

    hide grub at startup

--- boot.nix
+++ boot.nix
@@ -1,13 +1,21 @@
 { config, ... }:
 
 {
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub = {
-      enable = true;
-      version = 2;
-      efiSupport = true;
-      device = "nodev";
+  boot = {
+    kernelParams = [ "quiet" "splash" ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      timeout = 0;
+      grub = {
+        enable = true;
+        version = 2;
+        efiSupport = true;
+        device = "nodev";
+        extraConfig = ''
+          GRUB_HIDDEN_TIMEOUT=0
+          GRUB_HIDDEN_TIMEOUT_QUIET=true
+        '';
+      };
     };
   };
 }
