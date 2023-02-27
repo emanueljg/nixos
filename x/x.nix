8ffb4355e01a6e62d25225a25e219ff22ba5e0f3
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-27 18:17:13 +0100

    fix { }

--- x/x.nix
+++ x/x.nix
@@ -10,8 +10,9 @@
       autoLogin = {
         user = "ejg";
         enable = true;
+      };
+    };
   };
-};
   # allows for font installation
   # through home.packages
   my.fonts.fontconfig.enable = lib.mkForce true;
