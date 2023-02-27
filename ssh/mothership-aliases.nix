c8c25d00e4034fccc302f54047f0f571781a72c7
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-27 19:19:48 +0100

    add mothership alias

--- ssh/mothership-aliases.nix
+++ ssh/mothership-aliases.nix
@@ -4,4 +4,7 @@
 {
   my.home.shellAliases."msh" = 
     "ssh ejg@192.168.0.2 -i ~/.ssh/id_rsa_mothership";
+
+  my.home.shellAliases."scsh" = 
+    "ssh ejg@192.168.0.4 -i ~/.ssh/id_rsa_mothership";
 }
