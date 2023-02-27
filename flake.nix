0f6d20a4144456a173f38f39a15414c5f35c6efa
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-27 18:46:10 +0100

    make seneca flake point toward new hosts list

--- flake.nix
+++ flake.nix
@@ -89,60 +89,7 @@
     nixosConfigurations.seneca = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
-      modules = [ 
-				./seneca.nix
-
-				./enable-flakes.nix
-				./allow-unfree.nix
-        ./hm.nix
-				./misc-pkgs.nix
-				./state.nix
-
-				./hardware.nix
-				./stay-awake.nix
-				./drive.nix
-				./boot.nix
-				./hibernation.nix
-				./sound.nix
-				./locale.nix
-				./stateful-network.nix
-
-				./user.nix
-
-				./aliases.nix
-				./neovim.nix
-				./zsh.nix
-				./pfetch.nix
-        ./jq.nix
-
-				./git.nix
-				./python.nix
-				./java.nix
-        ./nodejs.nix
-				./mysql.nix
-
-				./pyradio.nix
-
-        ./x/x.nix
-          ./x/i3.nix
-          ./x/st/st.nix
-
-          ./x/qutebrowser/qutebrowser.nix
-          # upstream broke this
-          #./x/qutebrowser/overlay.nix
-          ./x/qutebrowser/quickmarks.nix
-          ./x/qutebrowser/translate.nix
-
-          ./x/picom.nix
-          ./x/pywal/wallpaper.nix
-          ./x/pywal/pywalQute.nix
-
-			
-				  ./x/latex.nix
-          ./x/android.nix
-
-				./kitchensink.nix
-      ];
+      modules = import ./hosts/seneca/seneca.nix;
     };
   };
 }
