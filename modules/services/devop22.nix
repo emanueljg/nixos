d2fec45150a2cc72ff05ca645d31802519ae9c4d
emanueljg <emanueljohnsongodin@gmail.com>
2023-03-27 03:31:23 +0200

    enable stack1

--- modules/services/devop22.nix
+++ modules/services/devop22.nix
@@ -45,10 +45,10 @@
     ];
 
     stack1 = {
-      enable = false;
+      enable = true;
     };
     stack2 = {
-      enable = true;
+      enable = false;
     };
     stack3 = {
       enable = false;
