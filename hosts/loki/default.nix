d98c9de7ddfceae644f48c0af7888e9a771b945b
emanueljg <emanueljohnsongodin@gmail.com>
2023-03-28 19:52:49 +0200

    do the loki part

--- hosts/loki/default.nix
+++ hosts/loki/default.nix
@@ -2,6 +2,7 @@ with (import ../../lib/funcs.nix); mkhost "loki" {
   blueprints = [ "core.nix" ];
   modules = [ 
     "services/devop22.nix"
+    "services/loki-autobuilder.nix"
   ];
 }
 
---
+++ modules/services/loki-autobuilder.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }:
+
+{
+  systemd = let name = "loki-autobuilder"; in {
+    timers.${name} = {
+      wantedBy = [ "timers.target" ];
+      after = [ "network-online.target" ];
+      timerConfig = {
+        OnCalendar="*-*-* *:*:00";
+        Unit = "${name}.service";
+      }
+    };
+
+    services.${name} = with pkgs; {
+      path = [
+        git
+        nixos-rebuild
+      ];
+      script = ''
+        git pull
+        chown -R ejg:users /etc/nixos
+        ${nixos-rebuild}/bin/nixos-rebuild --update-input app1-infrastruktur --update-input https-server-proxy --update-input nodehill-home-page --update-input devop22 --no-write-lock-file switch
+      '';
+      serviceConfig = {
+        WorkingDirectory = "/etc/nixos";
+      };
+    };
+  };
+}
