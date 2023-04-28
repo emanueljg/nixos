0edb626cf9d7b065658e45fb2a4b683fb199f658
emanueljg <emanueljohnsongodin@gmail.com>
2023-04-28 12:52:03 +0200

    make helix default editor

--- modules/programs/helix.nix
+++ modules/programs/helix.nix
@@ -1,6 +1,19 @@
-{ ... }:
+{ config, ... }:
 
 {
+
+  my.home.sessionVariables = rec {
+    EDITOR = "hx"; 
+    SUDO_EDITOR = EDITOR;
+    VISUAL = EDITOR;
+  };
+
+  # hack for setting editor as the EDITOR for login shells
+  # due to upstream bug
+  my.programs.zsh.initExtra = ''
+    export EDITOR="${config.my.home.sessionVariables.EDITOR}"
+  '';
+
   my.programs.helix = {
     enable = true;
   };
