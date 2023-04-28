fea351acdd8d8a94121a67ba847d6c72b5ca7c67
emanueljg <emanueljohnsongodin@gmail.com>
2023-04-28 12:52:39 +0200

    add qute-pass I forgotten

--- modules/programs/x/qutebrowser/qute-pass.nix
+++ modules/programs/x/qutebrowser/qute-pass.nix
@@ -3,9 +3,9 @@
 {
 
   my.programs.qutebrowser.keyBindings.normal = {
-    "zl" = "spawn --userscript qute-pass -U secret -u \"login: (.+)\" -d dmenu";
-    "zpl" = "spawn --userscript qute-pass -U secret -u \"login: (.+)\" -w -d dmenu";
-    "zul" = "spawn --userscript qute-pass -U secret -u \"login: (.+)\" -e -d dmenu";
+    "zl" = "spawn --userscript qute-pass -U secret -u \"user: (.+)\" -d dmenu";
+    "zpl" = "spawn --userscript qute-pass -U secret -u \"user: (.+)\" -w -d dmenu";
+    "zul" = "spawn --userscript qute-pass -U secret -u \"user: (.+)\" -e -d dmenu";
   };
 
 }
