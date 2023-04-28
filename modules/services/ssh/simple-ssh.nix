9b7f1916f37d32699d3223c60bfae83bec6d3895
emanueljg <emanueljohnsongodin@gmail.com>
2023-04-28 03:08:25 +0200

    fix

--- modules/services/ssh/simple-ssh.nix
+++ modules/services/ssh/simple-ssh.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 # simple ssh configuration for when you are in the middle
 # of setting up ssh and just want it to work.
@@ -8,8 +8,8 @@
   imports = [ ./_ssh.nix ];
 
   services.openssh.settings = {
-    PasswordAuthentication = true;
-    KbdInteractiveAuthentication = true; 
+    PasswordAuthentication = lib.mkForce true;
+    KbdInteractiveAuthentication = lib.mkForce true; 
   };
 
 }
