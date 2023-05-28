53468483a6710eab29f0473c792d1f03abc38483
emanueljg <emanueljohnsongodin@gmail.com>
2023-05-28 16:06:43 +0200

    quiet down direnv

--- modules/programs/direnv.nix
+++ modules/programs/direnv.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }: {
+
   my.programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
+
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+
+  my.home.sessionVariables = {
+    DIRENV_LOG_FORMAT = "";
+  };
+
 }
