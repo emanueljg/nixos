e4b5116c626ec86674d01114a1dcac1454006bb8
emanueljg <emanueljohnsongodin@gmail.com>
2023-06-14 00:13:51 +0200

    use discordo token with sops

--- modules/programs/discordo.nix
+++ modules/programs/discordo.nix
@@ -1,5 +1,19 @@
 { config, pkgs, discordo, ... }:
 
-{
-  environment.systemPackages = [ discordo.defaultPackage.x86_64-linux ];
+let secret = "discordo-token"; in {
+
+  sops.secrets.${secret} = {
+    sopsFile = ../../secrets/${secret}.yaml;
+    mode = "0440";
+    owner = "ejg";
+    group = "wheel";
+  };
+
+  my.home.packages = [(
+    pkgs.writeShellScriptBin "discordo" ''
+      ${discordo.defaultPackage.${pkgs.system}}/bin/discordo \
+        --token $(cat ${config.sops.secrets.${secret}.path})
+    ''
+  )];
+
 }
