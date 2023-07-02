e4559fdc1c671f75f4acf6b85b24f0f050d8866e
emanueljg <emanueljohnsongodin@gmail.com>
2023-07-02 19:10:14 +0200

    add mailserver stuff

---
+++ modules/services/mailserver.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }: let 
+  release = "nixos-23.05";
+  secret = "mailserver-ejg-password";
+in {
+  imports = [
+    (builtins.fetchTarball {
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/${release}/nixos-mailserver-${release}.tar.gz";
+      # This hash needs to be updated
+      sha256 = "sha256:1ngil2shzkf61qxiqw11awyl81cr7ks2kv3r3k243zz7v2xakm5c";
+    })
+  ];
+
+  sops.secrets.${secret} = {
+    sopsFile = ../../secrets/crown/${secret}.yaml;
+    mode = "0440";
+    owner = "ejg";
+    group = "wheel";
+  };
+
+  mailserver = {
+    enable = true;
+    fqdn = "emanueljg.com";
+    domains = [ "emanueljg.com" ];
+    loginAccounts = {
+      "ejg@emanueljg.com" = {
+         hashedPasswordFile = config.sops.secrets.${secret}.path;
+       };
+    };
+  };
+
+  
+}
+
