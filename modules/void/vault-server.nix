a9f0f554ffd875c278782f773fef27b12bcc064a
emanueljg <emanueljohnsongodin@gmail.com>
2024-01-16 19:11:58 +0100

    rm old vault server

--- modules/void/vault-server.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  services.vault = {
-    enable = true;
-    dev = true;
-    package = pkgs.vault-bin; # includes UI
-    devRootTokenID = "foobarbaz";
-  };
-  environment.systemPackages = with pkgs; [ vault ];
-}
