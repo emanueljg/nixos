b4d4870883a092af48dcd71093da21762f1a8029
emanueljg <emanueljohnsongodin@gmail.com>
2024-03-16 01:20:08 +0100

    update garnix

--- modules/base/garnix.nix
+++ modules/base/garnix.nix
@@ -1,6 +1,7 @@
 _: {
   nix.settings = {
     trusted-public-keys = [
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
     substituters = [
