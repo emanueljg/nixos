cef8d52e5c8db567931c99483eda1ece5d815a78
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2024-08-22 01:13:27 +0200

    add BA (#119)

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }: {
   programs.steam.enable = true;
+  virtualisation.waydroid.enable = true;
   environment.systemPackages = with pkgs; [
     wineWowPackages.waylandFull
     lutris
+    (pkgs.writeShellApplication {
+      name = "blue-archive";
+      text = "waydroid app launch com.nexon.bluearchive";
+    })
   ];
-  virtualisation.waydroid.enable = true;
 }
