43e05f1b61f5aef4db71e42dc4d3b6a46bde6bf1
emanueljg <emanueljohnsongodin@gmail.com>
2025-05-17 00:40:48 +0200

    add kavita

--- nixos/kavita.nix
+++ nixos/kavita.nix
@@ -1,5 +1,13 @@
-{
+{ config, self, ... }: {
+  sops.secrets."kavita-token-key" = {
+    sopsFile = "${self}/secrets/${config.networking.hostName}/kavita.yml";
+    mode = "0440";
+    owner = config.services.kavita.user;
+    group = config.services.kavita.user;
+  };
+
   services.kavita = {
     enable = true;
+    tokenKeyFile = config.sops.secrets.kavita-token-key.path;
   };
 }
