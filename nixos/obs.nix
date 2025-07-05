a1cb9ec60cbc7c8007270130c025f2316ab1a523
emanueljg <emanueljohnsongodin@gmail.com>
2025-07-05 04:30:40 +0200

    add obs

---
+++ nixos/obs.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  programs.obs-studio = {
+    enable = true;
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vkcapture
+    ];
+  };
+}
