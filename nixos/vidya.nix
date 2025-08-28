544ddb85115b988404f7e2a427f85d6edab70836
emanueljg <emanueljohnsongodin@gmail.com>
2025-08-28 22:09:24 +0200

    add katawa shoujo

--- nixos/vidya.nix
+++ nixos/vidya.nix
@@ -1,4 +1,6 @@
-{ other, lib, ... }: {
+{ other, lib, pkgs, ... }: {
+  local.allowed-unfree.names = [ "katawa-shoujo" ];
   environment.systemPackages =
-    builtins.attrValues (lib.removeAttrs other.vidya [ "utils" "zzz" ]);
+    (builtins.attrValues (lib.removeAttrs other.vidya [ "utils" "zzz" ]))
+    ++ [ pkgs.katawa-shoujo ];
 }
